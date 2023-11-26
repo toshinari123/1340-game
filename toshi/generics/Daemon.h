20dcfbf820f02e807ba715ac9658a5340a3577cc
root <root@LAPTOP-FOGUKVIB>
2023-11-26 12:53:23 +0800

    fixed bugs

--- toshi/generics/Daemon.h
+++ toshi/generics/Daemon.h
@@ -5,79 +5,101 @@
 #include "Layer.h"
 using namespace std;
 
+extern atomic_int daemon_to_terminate;
+extern atomic_bool daemon_terminated;
+
 class Daemon {
 private:
-    //modify below 2 functions in your custom daemon
-    virtual bool process_keystroke(queue<string>* errors, vector<Layer>* layers, int key) {
-        switch (key) {
+    //modify below 3 functions in your custom daemon
+    virtual bool process_keystroke(queue<wstring>* errors, int key) {
+        int smth = 0;
+        if (key == smth) {
             //add cases
-                default:
-                //modify this if necessary
-                key_to_pass_down = key;
-                return true;
-                break;
+            return false;
+        } else {
+            //modify this if necessary
+            key_to_pass_down = key;
+            return true;
         }
         return true;
     }
-    virtual bool process_error(queue<string>* errors, vector<Layer>* layers, string error) {
-        if (error.find("!!!END!!!") != string::npos) {
+    virtual bool process_error(queue<wstring>* errors, wstring error) {
+        if (error.find(L"!!!END!!!") != string::npos) {
             delete subdaemon;
         }
-        error_to_pass_up = "Generic_Daemon: " + error;
+        error_to_pass_up = L"Generic_Daemon: " + error;
         return true;
     }
-    //modify above 2 functions in your custom daemon
-    void run(vector<Layer>* layers,
-            queue<int>* keystrokes_from_up,
-            queue<string>* errors_to_pass_up) {
-        while (true) {
-            if (!keystrokes_from_up -> empty()) {
-                if (process_keystroke(errors_to_pass_up, layers, keystrokes_from_up -> front())) {
+    virtual void postprocess(queue<wstring>* errors) {
+        //spawn a new daemon maybe
+    }
+    //modify above 3 functions in your custom daemon
+    void run(queue<int>* keystrokes_from_up, queue<wstring>* errors_to_pass_up) {
+        signal(SIGSEGV, daemon_handle_int);
+        while (!preprocessed) {}
+        while (daemon_to_terminate != daemon_id) {
+            int dummy = keystrokes_from_up -> size();
+            if (!(keystrokes_from_up -> empty())) {
+                if (process_keystroke(errors_to_pass_up, keystrokes_from_up -> front())) {
                     keystrokes_to_pass_down -> push(key_to_pass_down);
                 }
                 keystrokes_from_up -> pop();
             }
-            if (!errors_from_down_and_self -> empty()) {
-                if (process_error(errors_to_pass_up, layers, errors_from_down_and_self -> front())) {
+            if (!(errors_from_down_and_self -> empty())) {
+                if (process_error(errors_to_pass_up, errors_from_down_and_self -> front())) {
                     errors_to_pass_up -> push(error_to_pass_up);
                 }
                 errors_from_down_and_self -> pop();
             }
+            //wcerr << daemon_id << L" alive\n";
         //this_thread::sleep_for(chrono::milliseconds(1));
         }
+        //terminate_subdaemon();
+        daemon_to_terminate = 0;
+        wcerr << L"HRLLIHLIHIH: " << to_wstring(daemon_id) << L" OK\n";
+        wcerr << L"What" << num_layers;
+        for (int i = 0; i < num_layers; i++) {
+            wcerr << L"YEY LAYER DLET\n";
+            auto temp = layers -> begin() + starting_layer;
+            layers -> erase(temp);
+        }
+        postprocess(errors_to_pass_up);
     }
     thread* daemon_thread;
-    vector<Layer>* layers_ref;
-    mutex layers_mutex;
 protected:
-    Daemon* subdaemon;
-    queue<int>* keystrokes_to_pass_down;
+    bool preprocessed;
+    Daemon* superdaemon;
     int key_to_pass_down;
-    queue<string>* errors_from_down_and_self;
-    string error_to_pass_up;
-public:
+    wstring error_to_pass_up;
+    vector<Layer>* layers;
     int starting_layer;
     int num_layers;
-    bool subdaemon_active;
-    Daemon(vector<Layer>* layers, int num_layers_in,
-            queue<int>* keystrokes_from_up, queue<string>* errors_to_pass_up) {
-        subdaemon_active = false;
-        layers_ref = layers;
+    int daemon_id;
+public:
+    Daemon* subdaemon;
+    string subdaemon_name;
+    queue<int>* keystrokes_to_pass_down;
+    queue<wstring>* errors_from_down_and_self;
+    Daemon(int daemon_id_in, vector<Layer>* layers_in, int num_layers_in,
+            queue<int>* keystrokes_from_up, queue<wstring>* errors_to_pass_up,
+            Daemon* superdaemon_in) {
+        subdaemon_name = "no subdaemons";
+        superdaemon = superdaemon_in;
+        layers = layers_in;
         starting_layer = layers -> size();
+        daemon_id = daemon_id_in;
         num_layers = num_layers_in;
         keystrokes_to_pass_down = new queue<int>;
-        errors_from_down_and_self = new queue<string>;
+        errors_from_down_and_self = new queue<wstring>;
         for (int i = 0; i < num_layers; i++) {
             Layer l(false, layers -> size());
-            lock_guard<mutex> guard(layers_mutex);
             layers -> emplace_back(l);
         }
-        daemon_thread = new thread(&Daemon::run, this, layers, 
-                keystrokes_from_up, errors_to_pass_up);
+        daemon_thread = new thread(&Daemon::run, this, keystrokes_from_up, errors_to_pass_up);
     }
     Daemon(const Daemon &d) {
         daemon_thread = d.daemon_thread;
-        layers_ref = d.layers_ref;
+        layers = d.layers;
         subdaemon = d.subdaemon;
         keystrokes_to_pass_down = d.keystrokes_to_pass_down;
         errors_from_down_and_self = d.errors_from_down_and_self;
@@ -87,7 +109,7 @@ public:
     Daemon& operator=(const Daemon& d) {
         if (this != &d) {
             daemon_thread = d.daemon_thread;
-            layers_ref = d.layers_ref;
+            layers = d.layers;
             subdaemon = d.subdaemon;
             keystrokes_to_pass_down = d.keystrokes_to_pass_down;
             errors_from_down_and_self = d.errors_from_down_and_self;
@@ -97,15 +119,45 @@ public:
         return *this;
     }
     ~Daemon() {
-        for (int i = 0; i < num_layers; i++) layers_ref -> pop_back();
-        delete subdaemon;
-        delete daemon_thread;
+        if (subdaemon_name != "no subdaemons") {
+            delete subdaemon;
+            while (subdaemon_name != "no subdaemons") {
+
+            }
+        }
+        daemon_to_terminate = daemon_id;
     }
+    /*void terminate_only_cur_daemon() {
+        daemon_terminated = false;
+        daemon_to_terminate = daemon_id;
+        const auto now = chrono::steady_clock::now();
+        while (!daemon_terminated && chrono::steady_clock::now().time_since_epoch().count() - now.time_since_epoch().count() < 1000000000) {
+            wcerr << to_wstring(chrono::steady_clock::now().time_since_epoch().count() - now.time_since_epoch().count()) << endl;
+        }
+        wcerr << L"AAA" << daemon_terminated << L"AAA\n";
+        if (daemon_terminated) {
+            wcerr << L"HRLLIHLIHIH: " << to_wstring(daemon_id) << L" OK\n";
+            daemon_to_terminate = 0;
+            daemon_terminated = false;
+            wcerr << L"What" << num_layers;
+            for (int i = 0; i < num_layers; i++) {
+                wcerr << L"YEY LAYER DLET\n";
+                auto temp = layers -> begin() + starting_layer;
+                layers -> erase(temp);
+            }
+            return;
+        }
+        errors_from_down_and_self -> push(L"daemon " + to_wstring(daemon_id) + L" took too long to terminate");
+    }*/
     void redraw() {
         for (int i = 0; i < num_layers; i++) {
-            layers_ref -> at(starting_layer + i).redraw(errors_from_down_and_self);
+            layers -> at(starting_layer + i).redraw(errors_from_down_and_self);
         }
-        if (subdaemon_active) subdaemon -> redraw();
+        if (subdaemon_name != "no subdaemons") subdaemon -> redraw();
+    }
+    static void daemon_handle_int(int sig) {
+        signal(SIGSEGV, SIG_IGN);
+        terminate();
     }
 };
 
--- toshi/generics/TGF_Object.h
+++ toshi/generics/TGF_Object.h
@@ -6,22 +6,6 @@
 using namespace std;
 
 class TGF_Object {
-private:
-    void check_row(queue<string>* errors, wstring s, string p, int row, string path) {
-        if (bb_cols == 0) bb_cols = s.size();
-        if (bb_cols != s.size()) {
-            errors -> push("TGF_Object_check_row: row length mismatch in row " + to_string(row) + " in file " + path);
-            return;
-        }
-        for (char c : s) {
-            int s = 0;
-            if (p == "*") s = 1;
-            else for (char d : p) s += (c == d);
-            if (s == 0) {
-                errors -> push("TGF_Object_check_row: character is not one of " + p + "in row " + to_string(row) + " of file " + path);
-            }
-        }
-    }
 public:
     int bb_rows, bb_cols;
     int pp_row, pp_col;
@@ -31,14 +15,31 @@ public:
     vector<int> opacity, foreground, background;
     vector<wchar_t> graphics;
     bool is_braille;
-    string optional_string;
-    TGF_Object(queue<string>* errors, string path, double ax, double ay, int ox, int oy) {
-        bb_cols = bb_rows = pp_row = pp_col = alignx = aligny = offsetx = offsety = br_offsetx = br_offsety = 0;
+    wstring optional_string;
+private:
+    void check_row(queue<wstring>* errors, wstring s, wstring p, int row, string path) {
+        if (bb_cols == 0) bb_cols = s.size();
+        if (bb_cols != s.size()) {
+            errors -> push(L"TGF_Object_check_row: row length mismatch in row " + to_wstring(row) + L" in file " + s2ws(path));
+            return;
+        }
+        for (wchar_t c : s) {
+            int s = 0;
+            if (p == L"*") s = 1;
+            else for (wchar_t d : p) s += (c == d);
+            if (s == 0) {
+                errors -> push(L"TGF_Object_check_row: character is not one of " + p + L"in row " + to_wstring(row) + L" of file " + s2ws(path));
+            }
+        }
+    }
+public:
+    void load_file(queue<wstring>* errors, string path) {
+        bb_cols = bb_rows = pp_row = pp_col = 0;
         wifstream f;
         f.imbue(locale(locale(), new codecvt_utf8<wchar_t>));
         f.open(path);
         if (f.bad()) {
-            errors -> push("TGF_Object_file_constructor: invalid TGF_Object file path " + path);
+            errors -> push(L"TGF_Object_file_constructor: invalid TGF_Object file path " + s2ws(path));
             return;
         }
         int cur_row = 0;
@@ -46,18 +47,18 @@ public:
         getline(f, s);
         if (s[s.size() - 1] == '\r') s.pop_back();
         cur_row++;
-        if (ws2s(s) != "[position_point]") {
-            errors -> push("TGF_Object_file_constructor: first row is not [position_point] in " + path);
+        if (s != L"[position_point]") {
+            errors -> push(L"TGF_Object_file_constructor: first row is not [position_point] in " + s2ws(path));
             return;
         }
         int count = 0;
         while (getline(f, s)) {
             if (s[s.size() - 1] == '\r') s.pop_back();
             if (s.empty()) break;
-            this -> check_row(errors, s, "01", cur_row, path);
+            this -> check_row(errors, s, L"01", cur_row, path);
             cur_row++;
             for (int j = 0; j < s.size(); j++) {
-                if (s[j] == '1') {
+                if (s[j] == L'1') {
                     count += 1, pp_row = bb_rows, pp_col = j;
                 }
             }
@@ -65,86 +66,99 @@ public:
         }
         for (int i = 0; i < bb_rows; i++) {
             for (int j = 0; j < bb_cols; j++) {
-                graphics.push_back(0);
+                graphics.push_back(L' ');
                 opacity.push_back(0);
                 background.push_back(0);
                 foreground.push_back(0);
             }
         }
         if (count == 0) {
-            errors -> push("TGF_Object_file_constructor: no position point specified in " + path);
+            errors -> push(L"TGF_Object_file_constructor: no position point specified in " + s2ws(path));
         }
         if (count > 1) {
-            errors -> push("TGF_Object_file_constructor: multiple position points specified in " + path);
+            errors -> push(L"TGF_Object_file_constructor: multiple position points specified in " + s2ws(path));
         }
         while (true) {
-            getline(f, s);
-            if (s.empty()) break;
-            if (s[s.size() - 1] == '\r') s.pop_back();
-            string p = "01";
-            if (s[0] != '[' || s[s.size() - 1] != ']') {
-                errors -> push("TGF_Object_file_constructor: section heading format wrong in row " + to_string(cur_row) + " of " + path);
+            if (!getline(f, s)) break;
+            if (s[s.size() - 1] == L'\r') s.pop_back();
+            wstring p = L"01";
+            if (s[0] != L'[' || s[s.size() - 1] != L']') {
+                errors -> push(L"TGF_Object_file_constructor: section heading format wrong in row " + to_wstring(cur_row) + L" of " + s2ws(path));
             }
             cur_row++;
-            if (ws2s(s) == "[graphics]") p = "*";
-            if (ws2s(s) == "[foreground_color]" || ws2s(s) == "[background_color]") p = "BWrgbcym";
+            if (s == L"[graphics]") p = L"*";
+            if (s == L"[foreground_color]" || s == L"[background_color]") p = L"BWrgbcym";
             wstring arr[bb_rows];
             for (int i = 0; i < bb_rows; i++) {
                 cur_row++;
-                getline(f, arr[i], L'\n');
+                getline(f, arr[i]);
                 wcerr << arr[i].size() << ": " << arr[i] << endl;
                 if (arr[i].empty()) {
-                    errors -> push("TGF_Object_file_constructor: missing line " + to_string(i) + " of section " + ws2s(s) + " in file " + path);
+                    errors -> push(L"TGF_Object_file_constructor: missing line " + to_wstring(i) + L" of section " + s + L" in file " + s2ws(path));
                     break;
                 }
-                if (arr[i][arr[i].size() - 1] == '\r') arr[i].pop_back();
+                if (arr[i][arr[i].size() - 1] == L'\r') arr[i].pop_back();
                 this -> check_row(errors, arr[i], p, cur_row, path);
             }
-            if (ws2s(s) == "[graphics]") {
+            if (s == L"[graphics]") {
                 for (int i = 0; i < bb_rows; i++) {
                     for (int j = 0; j < bb_cols; j++) {
                         graphics[i * bb_cols + j] = arr[i][j];
                     }
                 }
-            } else if (ws2s(s) ==  "[opacity]") {
+            } else if (s ==  L"[opacity]") {
                 for (int i = 0; i < bb_rows; i++) {
                     for (int j = 0; j < bb_cols; j++) {
-                        opacity[i * bb_cols + j] = (arr[i][j] == '1');
+                        opacity[i * bb_cols + j] = (arr[i][j] == L'1');
                     }
                 }
-            } else if (ws2s(s) == "[foreground_color]") {
+            } else if (s == L"[foreground_color]") {
 
-            } else if (ws2s(s) == "[background_color]") {
+            } else if (s == L"[background_color]") {
 
-            } else if (ws2s(s) == "[foreground_intensity]") {
+            } else if (s == L"[foreground_intensity]") {
 
-            } else if (ws2s(s) == "[background_intensity]") {
+            } else if (s == L"[background_intensity]") {
 
             }
             //work on colors later
             //https://www.linuxjournal.com/content/about-ncurses-colors-0
             if (!getline(f, s)) break;
-            if (s[s.size() - 1] == '\r') s.pop_back();
+            if (s[s.size() - 1] == L'\r') s.pop_back();
         }
+        wcerr << L"loaded: " << pp_row << L", " << pp_col << L"\n";
+    }
+    TGF_Object(queue<wstring>* errors, string path, double ax, double ay, int ox, int oy) {
         alignx = ax;
         aligny = ay;
         offsetx = ox;
         offsety = oy;
+        bb_cols = 0;
+        bb_rows = 0;
+        pp_col = 0;
+        pp_row = 0;
+        TGF_Object::load_file(errors, path);
     }
     //TODO: make constructor for braille
     //mvprintw no wrap
-    TGF_Object(queue<string>* errors, int x, int y, string s, ...) {
+    TGF_Object(queue<wstring>* errors, int x, int y, wstring s, ...) {
+        alignx = 0;
+        aligny = 0;
+        offsetx = x;
+        offsety = y;
+        bb_cols = 0;
+        bb_rows = 0;
+        pp_col = 0;
+        pp_row = 0;
         va_list vl;
         va_start(vl, s);
-        string line;
-        char out[10000];
-        vsprintf(out, s.c_str(), vl);
+        wstring line;
+        wchar_t out[10000];
+        vswprintf(out, 10000, s.c_str(), vl);
         optional_string = out;
         is_braille = false;
         alignx = aligny = 0;
-        offsetx = x;
-        offsety = y;
-        stringstream ss(s);
+        wstringstream ss(s);
         bb_rows = bb_cols = pp_row = pp_col = 0;
         while (getline(ss, line)) {
             bb_rows++;
@@ -152,14 +166,14 @@ public:
         }
         for (int i = 0; i < bb_rows; i++) {
             for (int j = 0; j < bb_cols; j++) {
-                graphics.push_back(0);
+                graphics.push_back(L' ');
                 opacity.push_back(0);
                 background.push_back(0);
                 foreground.push_back(0);
             }
         }
         int row = 0;
-        stringstream ss2(s);
+        wstringstream ss2(s);
         while (getline(ss2, line)) {
             for (int j = 0; j < line.size(); j++) {
                 graphics[row * bb_cols + j] = line[j];
@@ -169,18 +183,24 @@ public:
         va_end(vl);
     }
     //mvprintw with align coordinates (fractional)
-    TGF_Object(queue<string>* errors, double x, double y, string s, ...) {
-        va_list vl;
-        va_start(vl, s);
-        string line;
-        char out[10000];
-        vsprintf(out, s.c_str(), vl);
-        optional_string = out;
-        is_braille = false;
+    TGF_Object(queue<wstring>* errors, double x, double y, wstring s, ...) {
         alignx = x;
         aligny = y;
+        offsetx = 0;
+        offsety = 0;
+        bb_cols = 0;
+        bb_rows = 0;
+        pp_col = 0;
+        pp_row = 0;
+        va_list vl;
+        va_start(vl, s);
+        wstring line;
+        wchar_t out[10000];
+        vswprintf(out, 10000, s.c_str(), vl);
+        optional_string = out;
+        is_braille = false;
         offsetx = offsety = 0;
-        stringstream ss(s);
+        wstringstream ss(s);
         bb_rows = bb_cols = pp_row = pp_col = 0;
         while (getline(ss, line)) {
             bb_rows++;
@@ -188,14 +208,14 @@ public:
         }
         for (int i = 0; i < bb_rows; i++) {
             for (int j = 0; j < bb_cols; j++) {
-                graphics.push_back(0);
+                graphics.push_back(L' ');
                 opacity.push_back(0);
                 background.push_back(0);
                 foreground.push_back(0);
             }
         }
         int row = 0;
-        stringstream ss2(s);
+        wstringstream ss2(s);
         while (getline(ss2, line)) {
             for (int j = 0; j < line.size(); j++) {
                 graphics[row * bb_cols + j] = line[j];
@@ -205,42 +225,54 @@ public:
         va_end(vl);
     }
     //mvprintw wrap
-    TGF_Object(queue<string>* errors, int x, int y, int width, string s, ...) {
+    TGF_Object(queue<wstring>* errors, int x, int y, int width, wstring s, ...) {
+        alignx = 0;
+        aligny = 0;
+        offsetx = x;
+        offsety = y;
+        bb_cols = 0;
+        bb_rows = 0;
+        pp_col = 0;
+        pp_row = 0;
         va_list vl;
         va_start(vl, s);
-        string line;
-        char out[10000];
-        vsprintf(out, s.c_str(), vl);
+        wstring line;
+        wchar_t out[10000];
+        vswprintf(out, 10000, s.c_str(), vl);
         optional_string = out;
         is_braille = false;
         alignx = aligny = 0;
-        offsetx = x;
-        offsety = y;
-        stringstream ss(s);
+        wstringstream ss(out);
         bb_rows = pp_row = pp_col = 0;
         bb_cols = width;
         while (getline(ss, line)) {
-            bb_rows += line.size() / width + int(line.size() % width > 0);
+            bb_rows += line.size() / width + int((line.size() % width) > 0);
         }
         for (int i = 0; i < bb_rows; i++) {
             for (int j = 0; j < bb_cols; j++) {
-                graphics.push_back(0);
+                graphics.push_back(L' ');
                 opacity.push_back(1);
                 background.push_back(0);
                 foreground.push_back(0);
             }
         }
         int row = 0;
-        stringstream ss2(s);
+        wstringstream ss2(s);
         while (getline(ss2, line)) {
             for (int i = 0; i < line.size() / width; i++) {
-                for (int j = 0; j < (i == line.size() / width - 1 ? line.size() % width : width); j++) {
-                    graphics[row * bb_cols + j] = line[j];
+                for (int j = 0; j < ((i == (line.size() / width - 1)) ? line.size() % width : width); j++) {
+                    graphics[(x + row) * bb_cols + y + j] = line[i * bb_cols + j];
                 }
                 row++;
             }
         }
         va_end(vl);
+    }
+    ~TGF_Object() {
+       opacity.clear();
+       foreground.clear();
+       background.clear();
+       graphics.clear();
     }
     TGF_Object(const TGF_Object &o) {
         bb_rows = o.bb_rows;
@@ -260,7 +292,7 @@ public:
         is_braille = o.is_braille;
         optional_string = o.optional_string;
     }
-    TGF_Object& operator=(const TGF_Object& o) {
+    TGF_Object& operator=(const TGF_Object &o) {
         if (this != &o) {
             bb_rows = o.bb_rows;
             bb_cols = o.bb_cols;
