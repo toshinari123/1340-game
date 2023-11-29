e523a9a1e36e59054419a8964f27e545e30243c0
toshinari123 <68701054+toshinari123@users.noreply.github.com>
2023-11-29 16:17:45 +0800

    bugfix in run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 cd toshi
 make clean
 make game
-chmod u+x games/maze/game games/hangman/hangman games/gun/main games/blackjack/blackjack
+chmod u+x ./games/maze/game ./games/hangman/hangman ./games/gun/main ./games/blackjack/blackjack
 out/game 2> err.txt
