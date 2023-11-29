e0a996714d84dc056821e235673ebb81273d2c68
rishis.20 <rishishah202003@gmail.com>
2023-11-29 16:02:23 +0800

    trying

---
+++ Allfiles/main_gun_game.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ncurses.h>
+#include "gun_game.h"
+
+using namespace std;
+
+int main(){
+    //cout << "check 1" << endl;
+    initscr();
+    //cout << "check 2" << endl;
+    //Don't buffer input
+    cbreak();
+    //cout << "check 3" << endl;
+    //Enable input from special keys
+    //keypad(stdscr, TRUE);
+    //Don't echo input
+    //noecho();
+    //Don't display cursor
+    //curs_set(0);
+    //Update screen initially (prevents getch() from clearing the screen)
+    refresh();
+
+    //Set getch() to non-blocking mode
+    //nodelay(stdscr, TRUE);
+    game_main();
+    //game_main();
+    getch();
+    endwin();
+return 0;
+}
---
+++ Allfiles/popup.h
@@ -0,0 +1,11 @@
+#ifndef POPUP_H
+#define POPUP_H
+
+#include <ncurses.h>
+#include <string>
+
+using namespace std;
+
+int popup_main();
+
+#endif
