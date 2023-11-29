483e72ace644c3bbff05371aabc27c20d394e829
toshinari123 <68701054+toshinari123@users.noreply.github.com>
2023-11-29 15:57:33 +0800

    bash 2

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 #make clean
 make game
-chmod u+x games/maze/game games/hangman/hangman games/gun/main games/blackjack/blackjack
+chmod u+x toshi/games/maze/game toshi/games/hangman/hangman toshi/games/gun/main toshi/games/blackjack/blackjack
 out/game 2> err.txt
